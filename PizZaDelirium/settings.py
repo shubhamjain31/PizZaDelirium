22d81b0f81e415fc6d4d84adda9e06cb846ea56e
shubhamjain31 <sj27754@gmail.com>
2021-08-07 22:29:25 +0530

    Added django-decouple functionality

--- PizZaDelirium/settings.py
+++ PizZaDelirium/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qlib1c7r8c+t&&swlf4)&oyfnf@(oq1163=*hf^1xp)h_mizdc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
